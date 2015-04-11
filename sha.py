6c75e34bf0bd90f261daf679dd35f5029e2c205b
Eduard Bopp <eduard.bopp@aepsil0n.de>
2015-04-11 21:22:40 +0200

    Download and parse spectrum

--- sha.py
+++ sha.py
@@ -4,22 +4,25 @@ Spitzer Heritage Archive queries
 """
 
 import httplib2
-import StringIO
 
 
 DATA_SERVICE_URL = "http://sha.ipac.caltech.edu/applications/Spitzer/SHA/servlet/DataService"
 DATASET = "ivo://irsa.ipac/spitzer.level2"
 
 
-def parse_asteroid_obs(content):
+def parse_table(content):
     lines = content.split("\n")
+    headers_line = 0
+    while lines[headers_line][0] != "|":
+        headers_line += 1
     k, entries = 0, []
-    for entry in lines[0].split("|")[1:]:
-        entries.append([entry.strip(), k, len(entry) + 1 + k])
+    for entry in lines[headers_line].split("|")[1:]:
+        if entry != "":
+            entries.append([entry.strip(), k, len(entry) + 1 + k])
         k += len(entry) + 1
     return [
         {name: line[a:b].strip() for name, a, b in entries}
-        for line in lines[4:]
+        for line in lines[headers_line + 4:]
     ]
 
 
@@ -45,3 +48,6 @@ class SpitzerHeritageArchive(object):
         data_query = (DATA_SERVICE_URL + "?NAIFID={}&VERB=3&DATASET=" + DATASET
             ).format(naif_id)
         return self.http_backend.request(data_query, "GET")
+
+    def download_spectrum(self, observation):
+        return self.http_backend.request(observation["accessUrl"], "GET")
