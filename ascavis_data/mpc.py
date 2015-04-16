2047941963b19f7b1cb9b8059e2a0a921e604db4
Eduard Bopp <eduard.bopp@aepsil0n.de>
2015-04-16 21:09:56 +0200

    Refactor MySQL interface to MPC database

--- ascavis_data/mpc.py
+++ ascavis_data/mpc.py
@@ -1,97 +1,16 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+"""Minor Planet Center asteroid database handling
 
-# Parser fuer mp_properties, retrieved from http://minorplanetcenter.net/web_service on 11.4.2014 #
-# done by Katja :) #
+The database is available at http://minorplanetcenter.net/web_service in
+various formats. This module works with the SQL version of the database and
+retrieves specific data using SQL queries.
 
-import MySQLdb as mdb
+"""
+
+import pymysql as mdb
 import sys
-
-## needed to create query ##
-#max_amount_of_data = 1 #max number of data set to call (protect your poor computer, current DB size ~1GB)
-#include_none = 1 #include parameters with value 'none' (0/1) //not implemented yet
-
-parameters_to_limit = ['absolute_magnitude<15','mean_daily_motion>0.2'] #use [] for none (>/</=)
-#parameters_to_limit = []
-
-#find parameters with max / min, include DESC to find highest values #use [] for none 
-order_by = ['absolute_magnitude ASC']
-#order_by = []
-
-
-def make_query_mpc_db(max_amount_of_data, parameters_to_limit, order_by, columns="*"):
-    #(TODO: sanitize!!!)
-
-    #collect all conditional parts
-    query_condition = []
-    for n, (param) in enumerate(parameters_to_limit): #zip
-        query_condition.append(param)
-        if (n < len(parameters_to_limit)-1):
-            query_condition.append(' AND ')
-    query_condition = ''.join(query_condition)
-
-    #collect all order-by parts
-    query_order_by = []
-    for m, (param) in enumerate(order_by):
-        query_order_by.append(param)
-        if (m<len(order_by)-1):
-            query_order_by.append(',')
-    query_order_by = ''.join(query_order_by)
-
-    #collect all input to query
-    query = 'SELECT {} FROM properties'.format(columns)
-    if (len(parameters_to_limit) > 0):
-        query = (query + ' WHERE ('+str(query_condition)+')')
-    if (len(order_by) > 0):
-        query = (query + ' ORDER BY ' + query_order_by)
-    query = (query + ' LIMIT %s' % max_amount_of_data)
-
-    return(query)
-
-#########################################
-
-def retrieve_from_mpc_db(DB_SOURCE,DB_user,DB_pw,DB_name,query):
-    #retrieve data
-    try:
-        con = []
-        con = mdb.connect(DB_SOURCE, DB_user, DB_pw, DB_name)
-
-        cur = con.cursor(mdb.cursors.DictCursor)
-        cur.execute(query)
-
-        rows = cur.fetchall()
-        desc = cur.description
-        return rows
-
-        mpc_data = []
-        for row in rows:
-            #print(row['inclination_uncertainty'])
-            tmp = {}
-            mpc_data.append(row)
-
-        return(mpc_data)
-        
-        
-    except mdb.Error, e:
-      
-        print "Error %d: %s" % (e.args[0],e.args[1])
-        sys.exit(1)
-        
-    finally:    
-            
-        if con:    
-            con.close()
-
-########################################
-
-DB_SOURCE = "192.168.100.1"
-DB_user = "root"
-DB_pw = "space"
-DB_name = "mp_properties"
-
-#def query_mpc_db(DB_SOURCE,DB_user,DB_pw,DB_name,max_amount_of_data=100, parameters_to_limit=[], order_by=[]):
-    #TODO: make inputs optional!
 import string
+
+
 STANDARD_COLUMNS = string.join("""
 absolute_magnitude
 albedo  
@@ -153,15 +72,64 @@ v_minus_yprime
 v_minus_zprime
 """.split(), ",")
 
-def query_mpc_db(DB_SOURCE, DB_user, DB_pw, DB_name, max_amount_of_data=100,
-        parameters_to_limit=[], order_by=[]):
-    query = make_query_mpc_db(max_amount_of_data, parameters_to_limit, order_by, STANDARD_COLUMNS)
-    return retrieve_from_mpc_db(DB_SOURCE, DB_user, DB_pw, DB_name, query)
 
-def filter_script():
-    import simplejson as json
-    for i in range(7):
-        query = make_query_mpc_db("{} OFFSET {}".format(100000, 100000 * i), [], [], columns)
-        mpc_data = retrieve_from_mpc_db(DB_SOURCE,DB_user,DB_pw,DB_name,query)
-        with open("data_dump_{}.json".format(i), "w") as json_file:
-            json.dump(mpc_data, json_file)
+def mpc_db_query(max_amount_of_data=100, parameters_to_limit=[],
+        order_by=[], columns=STANDARD_COLUMNS):
+    """Generate an SQL query to retrieve data
+
+    TODO: sanitize!!
+
+    """
+    # Collect all conditional parts
+    query_condition = []
+    for n, param in enumerate(parameters_to_limit):
+        query_condition.append(param)
+        if n < len(parameters_to_limit) - 1:
+            query_condition.append(' AND ')
+    query_condition = ''.join(query_condition)
+
+    # Collect all order-by parts
+    query_order_by = []
+    for m, param in enumerate(order_by):
+        query_order_by.append(param)
+        if m < len(order_by) - 1:
+            query_order_by.append(',')
+    query_order_by = ''.join(query_order_by)
+
+    # Collect all input to query
+    query = 'SELECT {} FROM properties'.format(columns)
+    if len(parameters_to_limit) > 0:
+        query = (query + ' WHERE ('+str(query_condition)+')')
+    if len(order_by) > 0:
+        query = (query + ' ORDER BY ' + query_order_by)
+    query = (query + ' LIMIT %s' % max_amount_of_data)
+
+    return query
+
+
+class MpcSqlConnection(object):
+    """A connection to the MPC SQL database
+    
+    Use it like this:
+
+    with MpcSqlConnection(...) as con:
+        con.retrieve_data(query)
+
+    """
+
+    def __init__(self, address, user, password, db_name):
+        self.__con = mdb.connect(address, user, password, db_name)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, ty, value, traceback):
+        if self.__con:
+            self.__con.close()
+
+    def retrieve_data(self, query):
+        """Execute an SQL query on the database and retrieve the data"""
+        cur = self.__con.cursor(mdb.cursors.DictCursor)
+        cur.execute(query)
+        rows = cur.fetchall()
+        return rows
